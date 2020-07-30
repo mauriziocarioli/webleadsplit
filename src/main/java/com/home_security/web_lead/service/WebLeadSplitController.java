46096ecaa63bc50c06d513fcfdc08d7e93bc25e0
mauriziocarioli <maurizio.carioli@gmail.com>
2020-07-30 17:04:06 -0400

    debugging jpa

--- src/main/java/com/home_security/web_lead/service/WebLeadSplitController.java
+++ src/main/java/com/home_security/web_lead/service/WebLeadSplitController.java
@@ -1,6 +1,7 @@
 package com.home_security.web_lead.service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import com.home_security.web_lead.WebLeadSplit;
 
 @Controller
-@RequestMapping(value = "/api/webleadsplit")
+@RequestMapping(value = "/")
 public class WebLeadSplitController {
 
     private WebLeadSplitRepository repository;
@@ -26,34 +27,35 @@ public class WebLeadSplitController {
         return StreamSupport.stream(repository.findAll().spliterator(), false).collect(Collectors.toList());
     }
 
-    /*@ResponseStatus(HttpStatus.CREATED)
-    @PostMapping
-    public String createFruit(@RequestBody(required = false) WebLeadSplit webLeadSplit) {
-        repository.save(webLeadSplit);
-        return "redirect:/";
-    }*/
+    /*
+     * @ResponseStatus(HttpStatus.CREATED)
+     * 
+     * @PostMapping public String create(@RequestBody(required = false) WebLeadSplit
+     * webLeadSplit) { repository.save(webLeadSplit); return "redirect:/"; }
+     */
 
-    @GetMapping("/{id}")
-    public WebLeadSplit getWebLeadSplit(@PathVariable("id") Long id) {
-        return repository.findById(id)
-                            .orElse(null);
+    @GetMapping("/id/{id}")
+    public Optional <WebLeadSplit> get(@PathVariable("id") Long id) {
+        System.out.println(">>>>>>>>>>>> read by id = "+id);
+        return repository.findById(id);
+            //.orElse(null);
     }
 
-    @GetMapping("/{name}")
-    public WebLeadSplit getWebLeadSplit(@PathVariable("name") String name) {
-        return (WebLeadSplit)repository.findByName(name)
-        /* .orElse(null) */;
+    @GetMapping("/read/{name}")
+    public WebLeadSplit get(@PathVariable("name") String name) {
+        System.out.println(">>>>>>>>>>>> read by name = "+name);
+        return repository.lookupByName(name);
+            //.orElse(null);
     }
 
-    @PutMapping("/{id}")
-    public WebLeadSplit updateWebLeadSplit(@PathVariable("id") Long id, @RequestBody(required = false) WebLeadSplit w) {
-        //w.setId(id);
+    @PutMapping("/update/{name}")
+    public WebLeadSplit put(@PathVariable("name") String name, @RequestBody(required = false) WebLeadSplit w) {
         return repository.save(w);
     }
 
     /*@ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping("/{id}")
-    public void delete(@PathVariable("id") Long id) {
+    @DeleteMapping("/{name}")
+    public void delete(@PathVariable("name") String name) {
         repository.deleteById(id);
     }*/
 
--- src/main/java/com/home_security/web_lead/service/WebLeadSplitRepository.java
+++ src/main/java/com/home_security/web_lead/service/WebLeadSplitRepository.java
@@ -1,6 +1,8 @@
 package com.home_security.web_lead.service;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -8,11 +10,9 @@ import com.home_security.web_lead.WebLeadSplit;
 
 @Repository
 public interface WebLeadSplitRepository extends CrudRepository<WebLeadSplit, Long> {
-    @Query("select w from WebLeadSplit w where w.name = %?1")
-    List<WebLeadSplit> findByName(String name);
-    @Query("select w from WebLeadSplit w where w.name like %?1")
-    List<WebLeadSplit> findByNameLike(String name);
-    default List<WebLeadSplit> findAllWebLeadSplitsByName(String name) {
-        return findByName(name);
+    @Query("select w from WebLeadSplit w where w.name = ?1")
+    WebLeadSplit lookupByName(String name);
+    default Iterable<WebLeadSplit> findAllRows() {
+        return findAll();
     }
 }
